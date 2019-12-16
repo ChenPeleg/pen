3e850262283384a4a6e5b29a3f180e045782a0a4
Chen Peleg <chenpeleg@yahoo.com>
2019-12-16 15:33:47 +0200

    check errors part 1

--- js/penMain.js
+++ js/penMain.js
@@ -329,7 +329,7 @@ function buildWorkSheet (q) {
             answers.forEach (ans=>{
                 const ansNum = this.answer.indexOf(ans)
                 const ansLower = ans.toLowerCase()
-                bankDiv += `<span id="Q${this.number}A${ansNum}}" class="place-bank-element" ${seg}>${ans}</span>`
+                bankDiv += `<span id="Q${this.number}_A${ansNum}}" class="place-bank-element" ${seg}>${ans}</span>`
             })
             bankDiv += '</div>';
 
@@ -920,22 +920,33 @@ function checkAll(){
          nodeToinsertAfter.parentNode.insertBefore(newNode, nodeToinsertAfter )}
     const checkSvg = `<svg version="1.1"  x="0px" y="0px"
      	  viewBox="0 0 442.533 442.533" style="enable-background:new 0 0 442.533 442.533;"	 xml:space="preserve"> <g> 	<path d="M434.539,98.499l-38.828-38.828c-5.324-5.328-11.799-7.993-19.41-7.993c-7.618,0-14.093,2.665-19.417,7.993L169.59,247.248 l-83.939-84.225c-5.33-5.33-11.801-7.992-19.412-7.992c-7.616,0-14.087,2.662-19.417,7.992L7.994,201.852 C2.664,207.181,0,213.654,0,221.269c0,7.609, 2.664,14.088,7.994,19.416l103.351,103.349l38.831,38.828 c5.327,5.332, 11.8,7.994,19.414,7.994c7.611,0,14.084-2.669,19.414-7.994l38.83-38.828L434.539,137.33 c5.325-5.33,7.994-11.802,7.994-19.417C442.537,110.302,439.864,103.829,434.539,98.499z"><g><g><g><g><g><g><g><g> <g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><svg>`
+    const Xsvg = `<svg viewBox="0 0 24 24" style="fill:red;"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>`
     let amswerObj = {};
+    const isCorrect = (id, ans) =>{
+      const reg = /Q([0123456789]{1,3})(_A?([0123456789]{1,3}))?/i
+      const theMatch = id.match(reg);
+      const Qnum = theMatch ? Number(theMatch[1]) : false;
+      const PartNum = theMatch ? Number(theMatch[3]) : false;
+      return {Qnum,PartNum}
+    }
     const ansAdd = (name, value) =>{
+        L(isCorrect (name, ""))
         let good = Elm (name + '_mark' ,'span'); good.classList.add("check-mark");
-        good.innerHTML = checkSvg
+        good.innerHTML =   checkSvg //Xsvg
         amswerObj[name] = value
-        if (Id(name).type === 'text' || Id(name).type === 'radio') {
-             //good.style.position = 'relative';
-            //good.style.left = '0px';
+        if (Id(name).type === 'checkbox' ) {
+                good.style.left = '0px';
 
+        }
+        if (Id(name).type === 'radio' ) {
+            good.childNodes[0].style.top = '-10px';
         }
 
             insertAfter (good,Id(name))
 
         //
 
-        L(Id(name).parentNode)
+
 
     }
     const inputs = [...document.querySelectorAll('input')]
@@ -949,9 +960,9 @@ function checkAll(){
      ansAdd(o.id, ansRy)
     })
     inputs.forEach(i=>{
-        if (i.value !== i.defaultValue){
+        if (i.value !== i.defaultValue|| 1){
         ansAdd(i.id,i.value)
-    } else if (i.checked !==  i.defaultChecked){
+    } else if (i.checked !==  i.defaultChecked || 1){
         ansAdd(i.id, i.checked )
         }
     })
