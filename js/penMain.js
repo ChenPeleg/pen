bb9ad19314ff2fd50a24d6a7bd6c764f7f9b8799
Chen Peleg <chenpeleg@yahoo.com>
2019-12-19 19:37:00 +0200

    before messing with the menu hover activation

--- js/penMain.js
+++ js/penMain.js
@@ -514,6 +514,7 @@ function writePage (html = 'bla') {
 
 }
 function writeNavBarAndFooter () {
+    var fullscreenToggler = false;
 
     function clickNav (ev){
         const id = ev.target.id
@@ -536,10 +537,31 @@ function writeNavBarAndFooter () {
         nav.classList.add('hovering')
         setTimeout(()=>{nav.classList.remove('hovering')}, 1500)
     }
-    function openFullscreen() {
-  const elem = document.documentElement;
-  if (elem.requestFullscreen) {elem.requestFullscreen();}
-}
+    function toggleFullscreen() {
+      const elem = document.documentElement;
+      if (elem.requestFullscreen) {
+      if (!fullscreenToggler ) {
+        elem.requestFullscreen();
+        fullscreenToggler = true;
+
+      } else {
+
+        document.exitFullscreen()
+        fullscreenToggler =false;
+      }
+
+    let text = Id('fullscreenBtn').innerHTML;
+    let original = 'מסך מלא';
+    let replace = 'מסך רגיל';
+    if (!fullscreenToggler){[original,replace ]= [replace,original]}
+    text = text.replace(original,replace)
+
+    Id('fullscreenBtn').innerHTML = text
+
+  }
+    }
+
+    //document.addEventListener("fullscreenchange", function() {alert ('event')});
     const oldSVG = `<svg  width="20" height="20" viewBox="0 0 24 24"><path d="M24 9h-2v-4h-4v-2h6v6zm-6 12v-2h4v-4h2v6h-6zm-18-6h2v4h4v2h-6v-6zm6-12v2h-4v4h-2v-6h6z"/></svg>`;
     const fullScreenSVG = `<svg version="1.1" viewBox="0 0 36 36" class="fullscreen"><g>< xlink:href="#ytp-id-27"></use><path  d="m 10,16 2,0 0,-4 4,0 0,-2 L 10,10 l 0,6 0,0 z" id="ytp-id-27"></path></g><g><path d="m 20,10 0,2 4,0 0,4 2,0 L 26,10 l -6,0 0,0 z" id="ytp-id-28"></path></g><g><path d="m 24,24 -4,0 0,2 L 26,26 l 0,-6 -2,0 0,4 0,0 z" id="ytp-id-29"></path></g><g ><path class="ytp-svg-fill" d="M 12,20 10,20 10,26 l 6,0 0,-2 -4,0 0,-4 0,0 z" id="ytp-id-30"></path></g></svg>`;
     const allSects = [...document.querySelectorAll('section')]
@@ -591,7 +613,7 @@ function writeNavBarAndFooter () {
     navbar.appendChild(navbarhinter);
     const pageMetaContainer = Id('pageMetaContainer')
     pageMetaContainer.insertBefore(navbarContatiner, pageMetaContainer.childNodes[0])
-    Id('fullscreenBtn').addEventListener('click', openFullscreen)
+    Id('fullscreenBtn').addEventListener('click', toggleFullscreen)
 
 
 }
@@ -1194,11 +1216,6 @@ function saveState (){
     }
     for (let i = 1; i < G.V.length ;i++){
       if (G.V[i].typ.includes("q_") ){
-      G.V[i].save =  getInputFromAns(i);
-
-
-
-
       if (G.V[i].save === 0 || G.V[i].save) { L(i, G.V[i].save)}
       }
     }
