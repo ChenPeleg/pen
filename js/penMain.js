30632e737d0cae3080eed7d8e3827eefe596cc55
Chen Peleg <chenpeleg@yahoo.com>
2019-12-26 11:49:29 +0200

    progressbar and check button animation2

--- js/penMain.js
+++ js/penMain.js
@@ -265,6 +265,13 @@ function langSet() {
     };
 
 }
+function svgGetter(typ) {
+    const checkSvg = `<svg version="1.1"  x="0px" y="0px"
+       	  viewBox="0 0 442.533 442.533" style="enable-background:new 0 0 442.533 442.533;"	 xml:space="preserve"> <g> 	<path d="M434.539,98.499l-38.828-38.828c-5.324-5.328-11.799-7.993-19.41-7.993c-7.618,0-14.093,2.665-19.417,7.993L169.59,247.248 l-83.939-84.225c-5.33-5.33-11.801-7.992-19.412-7.992c-7.616,0-14.087,2.662-19.417,7.992L7.994,201.852 C2.664,207.181,0,213.654,0,221.269c0,7.609, 2.664,14.088,7.994,19.416l103.351,103.349l38.831,38.828 c5.327,5.332, 11.8,7.994,19.414,7.994c7.611,0,14.084-2.669,19.414-7.994l38.83-38.828L434.539,137.33 c5.325-5.33,7.994-11.802,7.994-19.417C442.537,110.302,439.864,103.829,434.539,98.499z"><g><g><g><g><g><g><g><g> <g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><svg>`
+    const Xsvg = `<svg viewBox="0 0 24 24" style="fill:red;"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>`
+    if (typ === 'good') { return checkSvg }
+    if (typ === 'bad') { return Xsvg }
+}
 
 function buildObjectsOfWorkSheet(q) {
     const elementOfhtml = (t) => {
@@ -1219,29 +1226,44 @@ function checkProcessWithAnimation() {
     const animationBar = Elm('animationBar')
     const checkTxt1 = Elm('checkTxt1'); // innerAnimationBar
     const checkTxt2 = Elm('checkTxt2'); // innerAnimationBar
-    const innerAnimationBar = Elm('innerAnimationBar')
-    const numOfQuestion = Object.keys(saveState()).length
-    L(numOfQuestion)
-    let fulltext = `מתוך ` + numOfQuestion + "  שאלות, ענית על: " + "<br>"
-    //fulltext += 
+    const checkClosebutton = Elm('checkClosebutton', 'span')
+    const innerAnimationBar = Elm('innerAnimationBar');
+    const boolArray = checkAll(false).map(e => e || false)
+    const allAnswers = boolArray.length;
+    const wrongAnswers = boolArray.filter(e => e === false).length;
+    const rightAnswers = allAnswers - wrongAnswers;
+    const goodMark = svgGetter('good')
+    const badMark = svgGetter('bad')
+
+    let fulltext = `מתוך ` + allAnswers + "  שאלות, ענית על: " + "<br>"
+    fulltext += `<span style="font-weight:bolder;"><span class="check-mark-general">${goodMark} </span> ` + rightAnswers + ' נכון מאד ' + "<br>"
+    fulltext += `<span class="check-mark-general">${badMark} </span> ` + wrongAnswers + ' לא נכון או חלקי ' + "</span><br><br>";
+    fulltext += 'נותרו עוד ' + G.saves.checks + ' בדיקות '
+
+
     checkTxt1.innerHTML = 'התשובות נבדקות ';
+    checkClosebutton.innerHTML = 'סגירה'
     animationBar.innerHTML = "  "
     animationBar.appendChild(innerAnimationBar)
     checkingProcess.appendChild(checkTxt1);
     checkingProcess.appendChild(animationBar)
     checkingProcess.appendChild(checkTxt2);
+    checkingProcess.appendChild(checkClosebutton);
 
 
     Id('pageMetaContainer').appendChild(checkingProcess)
     setTimeout(() => {
         innerAnimationBar.remove()
-        animationBar.classList.add('shrink-animation-bar')
+        animationBar.classList.add('shrink-animation-bar');
+        checkTxt2.innerHTML = fulltext;
+        checkAll(true)
 
-    }, 1000)
+
+    }, 100)
 }
-function checkAll() {
+function checkAll(toMark = true) {
     saveState()
-    let amswerObj = {};
+    let answerBoolArray = [];
     function getQnumber(id) {
         const reg = /Q([0123456789]{1,3})(_A?([0123456789]{1,3}))?/i
         const theMatch = id.match(reg);
@@ -1309,11 +1331,11 @@ function checkAll() {
         const insertAfter = function (newNode, nodeToinsertAfter) {//.nextSibling
             nodeToinsertAfter.parentNode.insertBefore(newNode, nodeToinsertAfter)
         }
-        const checkSvg = `<svg version="1.1"  x="0px" y="0px"
-       	  viewBox="0 0 442.533 442.533" style="enable-background:new 0 0 442.533 442.533;"	 xml:space="preserve"> <g> 	<path d="M434.539,98.499l-38.828-38.828c-5.324-5.328-11.799-7.993-19.41-7.993c-7.618,0-14.093,2.665-19.417,7.993L169.59,247.248 l-83.939-84.225c-5.33-5.33-11.801-7.992-19.412-7.992c-7.616,0-14.087,2.662-19.417,7.992L7.994,201.852 C2.664,207.181,0,213.654,0,221.269c0,7.609, 2.664,14.088,7.994,19.416l103.351,103.349l38.831,38.828 c5.327,5.332, 11.8,7.994,19.414,7.994c7.611,0,14.084-2.669,19.414-7.994l38.83-38.828L434.539,137.33 c5.325-5.33,7.994-11.802,7.994-19.417C442.537,110.302,439.864,103.829,434.539,98.499z"><g><g><g><g><g><g><g><g> <g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><g><svg>`
-        const Xsvg = `<svg viewBox="0 0 24 24" style="fill:red;"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>`
+        const checkSvg = svgGetter('good')
+        const Xsvg = svgGetter('bad')
 
         const check = checkIfAnsCorrect(getQnumber(name), value)
+        answerBoolArray.push(check)
 
         if (!Id(name)) { name += "_0" } // in the case of checkboxes
 
@@ -1342,7 +1364,7 @@ function checkAll() {
         if (Id(name).type === 'radio') {
             grade.childNodes[0].style.top = '-10px';
         }
-        insertAfter(grade, Id(name))
+        if (toMark) { insertAfter(grade, Id(name)) }
     }
     const inputs = [...document.querySelectorAll('input:not(.checkbox0)')]
     const orders = [...document.querySelectorAll('.orderList')]
@@ -1382,7 +1404,7 @@ function checkAll() {
         ansAdd(p, p)
     )
 
-
+    return answerBoolArray
 
 }
 function saveState() {
